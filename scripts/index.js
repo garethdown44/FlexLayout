8dbf42aca6459c8ea77b58a37a59d8a1033579bf
Gareth Down <g@garethdown.com>
2016-03-30 21:57:52 +0100

    create dist build

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+// module.exports.Layout = require('./view/Layout');
+// module.exports.Splitter = require('./view/Splitter');
+// module.exports.Tab = require('./view/Tab');
+// module.exports.TabButton = require('./view/TabButton');
+// module.exports.TabSet = require('./view/TabSet');
+
+// module.exports.Actions = require('./model/Actions');
+// module.exports.Model = require('./model/Model');
+// module.exports.Node = require('./model/Node');
+// module.exports.RowNode = require('./model/RowNode');
+// module.exports.SplitterNode = require('./model/SplitterNode');
+// module.exports.TabNode = require('./model/TabNode');
+// module.exports.TabSetNode = require('./model/TabSetNode');
+
+
+import Layout from './view/Layout'
+import Splitter from './view/Splitter'
+import Tab from './view/Tab'
+import TabButton from './view/TabButton'
+import TabSet from './view/TabSet'
+import Actions from './model/Actions'
+import Model from './model/Model'
+import Node from './model/Node'
+import RowNode from './model/RowNode'
+import SplitterNode from './model/SplitterNode'
+import TabNode from './model/TabNode'
+import TabSetNode from './model/TabSetNode'
+
+export {  Layout, 
+          Splitter,
+          Tab,
+          TabButton,
+          TabSet,
+          Actions,
+          Model,
+          Node,
+          RowNode,
+          SplitterNode,
+          TabNode,
+          TabSetNode
+        };
